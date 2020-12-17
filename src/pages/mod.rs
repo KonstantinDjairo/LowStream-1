865e45a46fca6b9379b8bdc5e42d394c070af3c6
André da Silva Freitas <andrecastanhal@Gmail.com>
2020-12-16 21:05:41 -0300

    Add files via upload

--- src/pages/mod.rs
+++ src/pages/mod.rs
@@ -3,3 +3,4 @@ pub mod posts;
 pub mod page_not_found;
 pub mod register;
 pub mod login;
+pub mod player;
---
+++ src/pages/player.rs
@@ -0,0 +1,56 @@
+use yew::prelude::*;
+
+pub struct Player;
+impl Component for Player
+{
+    type Message = ();
+    type Properties = ();
+
+    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self
+    {
+        Self
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender
+    {
+        unimplemented!()
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender
+    {
+        false
+    }
+
+    fn view(&self) -> Html
+    {
+        html!{
+            <>
+                <section class="hero is-medium is-light has-background" style="padding-top: 20px;">
+                    <img src="https://i.imgur.com/0ve4gmx.jpg" class="hero-background is-transparent"/>
+                        <div class="hero-body">
+                            <div class="container">
+                                <h1 class="title">
+                                    {"Shingeki no Kyojin"}
+                                </h1>
+                                <h2 class="subtitle">{"Final Season"} <a href="/router/authors/1443460666139885527" class="has-text-weight-semibold">
+                                    {": Ep. 1"}
+                                    </a>
+                                </h2>
+                                <span class="tag is-danger">
+                                    {"Ação"}
+                                </span>
+                                <div class="context has-text-centered" style="padding-top: 40px; padding-bottom: 40px;">
+                                    <video controls=true autoplay="" name="media" width="600" height="360" style="border-radius: 18px; box-shadow: 0px 0px 18px rgba(0, 0, 0, 70%)">
+                                        <source src="https://5.orezraey.workers.dev/0:/Animes/Shingeki%20no%20Kyojin:%20The%20Final%20Season/M%C3%BAltiplas%20Legendas%20-%201080p%252F720p%252F480p/1080p/Shingeki%20no%20Kyojin%20-%20The%20Final%20Season%20-%2001%20%5B1080p%5D%5BM%C3%BAltiplas%20Legendas%5D.mkv" type="video/mkv"/>
+                                    </video>
+                                </div>
+                            <div class="tags">
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+            </>
+        }
+    }
+}
