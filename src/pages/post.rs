b378324bbea401a0ddf57e78562c89c7f62a66d1
André da Silva Freitas <andrecastanhal@Gmail.com>
2021-01-10 19:25:49 -0300

    Beta de player SOMENTE TEST

--- src/pages/post.rs
+++ src/pages/post.rs
@@ -42,7 +42,8 @@ pub enum Msg {
     GetInfo,
     ReceiveResponse(Result<Struture, anyhow::Error>),
     GetOption(usize),
-    TogglePlay(String, String, String)
+    TogglePlay(String, String, String),
+    Close
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Properties)]
@@ -60,18 +61,39 @@ pub struct Eps
     number: usize,
     current_video: String,
     link_video: String,
-    type_video: String
+    type_video: String,
+    play: Html,
+    close: bool
 }
 
 impl Eps {
+    fn notification(&self) -> Html
+    {
+        if self.close
+        {
+            return html!{
+                <>
+                
+                <div class="notification is-danger is-light">
+                    // <button class="delete" onclick=self.link.callback(|_| Msg::Close)></button>
+                        <strong>{"O player ainda é um test, por conta disso a sua lógica ainda está em processo de desenvolvimento. Para reproduzir o próximo episódio, feche o player abaixo primeiramente, e por conseguinte clique no card desejado. Obg por sua visita uwu"}</strong>
+                </div>
+                <div class="notification  is-dark">
+                    <button class="delete" onclick=self.link.callback(|_| Msg::Close)></button>
+                        <strong>{self.current_video.clone()}</strong>
+                </div>
+                        {self.play.clone()}
+                </>
+            }
+        }
+        html!{}
+    }
     fn view_json(&self) -> Html {
         let mut cards: Vec<Html> = Vec::new();
         let mut options: Vec<Html> = Vec::new();
-        // let mut video = &self.current_video;
         // let mut type_video
         match self.json {
             Some(ref content) => {
-                
                 for j in 0..content.animes[self.name as usize].dados.len()
                 {
                     options.push(html!{
@@ -91,23 +113,24 @@ impl Eps {
                 // {
                 for i in 0..content.animes[self.name as usize].dados[self.number].eps.len()
                 {
+                        let video = content.animes[self.name as usize].dados[self.number].eps[i].name.clone();
+                        let player = content.animes[self.name as usize].dados[self.number].eps[i].player.clone();
+                        let type_video = content.animes[self.name as usize].dados[self.number].eps[i].type_video.clone();
                         cards.push(html!{
                             <li class="card" style="background: black; min-width: auto">
-                            <AppAnchor route=AppRoute::Player(content.animes[self.name as usize].dados[self.number].eps[i].player.clone(), 
-                                                              content.animes[self.name as usize].background.clone(), 
-                                                              content.animes[self.name as usize].dados[self.number].eps[i].name.clone(), 
-                                                              content.animes[self.name as usize].dados[self.number].eps[i].type_video.clone(),
-                                                              )>
-                            // <a onclick=self.link.callback(move |_| Msg::TogglePlay(content.animes[self.name as usize].dados[self.number].eps[i].name.clone(),
-                            //                                                        content.animes[self.name as usize].dados[self.number].eps[i].player.clone(),
-                            //                                                        content.animes[self.name as usize].dados[self.number].eps[i].type_video.clone()))>
+                            // <AppAnchor route=AppRoute::Player(content.animes[self.name as usize].dados[self.number].eps[i].player.clone(), 
+                            //                                   content.animes[self.name as usize].background.clone(), 
+                            //                                   content.animes[self.name as usize].dados[self.number].eps[i].name.clone(), 
+                            //                                   content.animes[self.name as usize].dados[self.number].eps[i].type_video.clone(),
+                            //                                   )>
+                            <a onclick=self.link.callback(move |_| Msg::TogglePlay(video.clone(), player.clone(), type_video.clone()))>
                                 // <a class="card-image" style=format!("background-image: url({});", content.animes[self.name as usize].background.clone()) loading="lazy">
                                 // </a>
                                 <a class="list">
                                     <strong><h2>{content.animes[self.name as usize].dados[self.number].eps[i].name.clone().replace(".mkv", " ").replace(".mp4", " ").replace(".avi", " ")}</h2></strong>
                                 </a>
-                                // </a>
-                            </AppAnchor>
+                                </a>
+                            // </AppAnchor>
 
                             </li>
                         });
@@ -119,23 +142,23 @@ impl Eps {
                     <>
                         <section class="hero is-small is-dark is-bold has-background">
                             <img src=content.animes[self.name as usize].background.clone() class="hero-background is-transparent" style=" filter: blur(6px)"/>
+                            <div class="cover-image-header__animes">
+                            <div class="cover-image-header__rows">
+                            </div>
+                        </div>
                             <div class="hero-body">
                                 <div class="container">
                                     <h2 class="title" style="padding-top: 80px;">
                                         {content.animes[self.name as usize].anime.clone()}
                                     </h2>
-                                    // <video::Video video=&self.current_video, 
-                                    //             type_video=&self.type_video />
+                                    {self.notification()}
                                     <nav style="z-index: 1000">
                                         <div class="navbar-item has-dropdown is-hoverable" style="background-color: rgba(0, 0, 0, 0%); backdrop-filter: blur(10px); border-radius: 8px;">
-                                            <a class="navbar-link" style="background-color: rgba(0, 0, 0, 0%); backdrop-filter: blur(10px); border-radius: 18px; color: white;">
+                                            <a class="navbar-link" style="background-color: #36363626; backdrop-filter: blur(10px); border-radius: 18px; color: white;">
                                                 {"Opções"}
                                             </a>
                                             <div class="navbar-dropdown is-up is-boxed" style="background-color: rgba(0, 0, 0);">
                                             {for options.clone()}
-                                            //    <a class="navbar-item" href="https://github.com/lowstream-community/LowStream" style="color: white">
-                                            //        {"GitHub"}
-                                            //    </a>
                                             </div>
                                         </div>
                                     </nav>
@@ -214,9 +237,11 @@ impl Component for Eps {
             json: None,
             error: None,
             number: 0,
-            current_video: String::from("none"),
-            link_video: String::from("none"),
-            type_video: String::from("none")
+            current_video: String::new(),
+            link_video: String::new(),
+            type_video: String::new(),
+            play: html!{},
+            close: false
         }
     }
 
@@ -224,10 +249,104 @@ impl Component for Eps {
         use Msg::*;
 
         match msg {
+            Close => {
+                self.close = false;
+                self.play = html!{};
+                true
+            }
             TogglePlay(name, link_video, type_video) => {
+                self.close = true;
                 self.current_video = name;
                 self.link_video = link_video;
                 self.type_video = type_video;
+                self.play = html!{
+                    <div class="context has-text-centered" style="padding-top: 40px; padding-bottom: 140px;">
+                        <video controls=true autoplay="" width="100%" height="550" style="border-radius: 18px; box-shadow: 0px 0px 18px rgba(0, 0, 0, 70%)">
+                            <source src=self.link_video.clone() type=self.type_video.clone()/>
+                        </video>
+                        <div class="notransition" style="background-color: rgba(0, 0, 0, 50%); color: white; display: inline-block;
+                                    width: 90%;
+                                    font-size: 1rem;
+                                    text-decoration: none;
+                                    overflow: hidden;
+                                    box-shadow: 0 0 4rem -1rem rgba(0, 0, 0, 1);
+                                    border-radius: 18px;">
+                                        <header class="card-header">
+                                            <p class="card-header-title" style="color: white"><span class="icon"><i aria-hidden="true" class="fa fa-play-circle"></i></span>{" Play in android "}<span class="icon"></span></p>
+                                        </header>
+                                        <div class="card-content">
+                                            <div >
+                                                <div class="columns is-mobile is-multiline has-text-centered">
+                                                    <div class="column">
+                                                        <p class="heading">
+                                                            <a href=format!("iina://weblink?url={}",self.link_video.clone())>
+                                                                <figure class="image" style="max-width: 40px; height: auto; margin: 0px auto;"><img src="https://cdn.jsdelivr.net/gh/Aicirou/goindex-theme-acrou@v2.0.8/dist/images/player/iina.png" class="icon"/></figure>
+                                                            </a>
+                                                        </p>
+                                                        <p>{"IINA"}</p>
+                                                    </div>
+                                                    <div class="column">
+                                                        <p class="heading">
+                                                            <a href=format!("potplayer://{}",self.link_video.clone())>
+                                                                <figure class="image" style="max-width: 40px; height: auto; margin: 0px auto;"><img src="https://cdn.jsdelivr.net/gh/Aicirou/goindex-theme-acrou@v2.0.8/dist/images/player/potplayer.png" class="icon"/></figure>
+                                                            </a>
+                                                        </p>
+                                                        <p>{"PotPlayer"}</p>
+                                                    </div>
+                                                    <div class="column">
+                                                        <p class="heading">
+                                                            <a href=format!("vlc://{}",self.link_video.clone())>
+                                                                <figure class="image" style="max-width: 40px; height: auto; margin: 0px auto;"><img src="https://cdn.jsdelivr.net/gh/Aicirou/goindex-theme-acrou@v2.0.8/dist/images/player/vlc.png" class="icon"/></figure>
+                                                            </a>
+                                                        </p>
+                                                        <p>{"VLC"}</p>
+                                                    </div>
+                                                    <div class="column">
+                                                        <p class="heading">
+                                                            <a href=format!("thunder://{}",self.link_video.clone())>
+                                                                <figure class="image" style="max-width: 40px; height: auto; margin: 0px auto;"><img src="https://cdn.jsdelivr.net/gh/Aicirou/goindex-theme-acrou@v2.0.8/dist/images/player/thunder.png" class="icon"/></figure>
+                                                            </a>
+                                                        </p>
+                                                        <p>{"Thunder"}</p>
+                                                    </div>
+                                                    <div class="column">
+                                                        <p class="heading">
+                                                            <a href="javascript:alert(&quot;暂未实现&quot;)">
+                                                                <figure class="image" style="max-width: 40px; height: auto; margin: 0px auto;"><img src="https://cdn.jsdelivr.net/gh/Aicirou/goindex-theme-acrou@v2.0.8/dist/images/player/aria2.png" class="icon"/></figure>
+                                                            </a>
+                                                        </p>
+                                                        <p>{"Aria2"}</p>
+                                                    </div>
+                                                    <div class="column">
+                                                        <p class="heading">
+                                                            <a href=format!("nplayer-{}",self.link_video.clone())>
+                                                                <figure class="image" style="max-width: 40px; height: auto; margin: 0px auto;"><img src="https://cdn.jsdelivr.net/gh/Aicirou/goindex-theme-acrou@v2.0.8/dist/images/player/nplayer.png" class="icon"/></figure>
+                                                            </a>
+                                                        </p>
+                                                        <p>{"nPlayer"}</p>
+                                                    </div>
+                                                    <div class="column">
+                                                        <p class="heading">
+                                                            <a href=format!("intent:{}#Intent;package=com.mxtech.videoplayer.ad;S.title=undefined;end",self.link_video.clone())>
+                                                                <figure class="image" style=" max-width: 40px; height: auto; margin: 0px auto;"><img src="https://cdn.jsdelivr.net/gh/Aicirou/goindex-theme-acrou@v2.0.8/dist/images/player/mxplayer.png" class="icon"/></figure>
+                                                            </a>
+                                                        </p>
+                                                        <p>{"MXPlayer(Free)"}</p>
+                                                    </div>
+                                                    <div class="column">
+                                                        <p class="heading">
+                                                            <a href=format!("intent:{}#Intent;package=com.mxtech.videoplayer.pro;S.title=undefined;end",self.link_video.clone())>
+                                                                <figure class="image" style=" max-width: 40px; height: auto; margin: 0px auto;"><img src="https://cdn.jsdelivr.net/gh/Aicirou/goindex-theme-acrou@v2.0.8/dist/images/player/mxplayer.png" class="icon" /></figure>
+                                                            </a>
+                                                        </p>
+                                                        <p>{"MXPlayer(Pro)"}</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                    </div>
+                };
                 true
             }
             GetOption(value) => {
